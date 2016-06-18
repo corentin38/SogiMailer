747a6199419613f3b4220cc6844fcb36dccf6fde
Corentin Marciau <corentin@marciau.fr>
2016-06-18 17:24:48 +0200

    [DEV] Add basic android studio project

---
+++ app/src/main/java/sogilis/com/sogimailer/MainActivity.java
@@ -0,0 +1,13 @@
+package sogilis.com.sogimailer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
