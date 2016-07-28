d05e671b6fbb2bf22b09170215766675651fa525
Corentin Marciau <corentin@sogilis.com>
2016-07-28 16:30:10 +0200

    Working with IntentService

--- app/src/main/java/com/sogilis/sogimailer/MainActivity.java
+++ app/src/main/java/com/sogilis/sogimailer/MainActivity.java
@@ -1,11 +1,8 @@
 package com.sogilis.sogimailer;
 
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.IBinder;
-import android.os.RemoteException;
+import android.os.Handler;
+import android.os.ResultReceiver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,65 +14,61 @@ public class MainActivity extends AppCompatActivity {
 
 	private static final String TAG = "SOGI---MAILER";
 
-	IHelloService service;
-	AddServiceConnection connection;
+	private MailerResult r;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		initService();
-	}
-
-	private void initService() {
-		Log.i(TAG, "initService()" );
-		connection = new AddServiceConnection();
-		Intent i = new Intent();
-		Log.d(TAG, "Class name : " + com.sogilis.sogimailer.SogiMailerService.class.getName());
-		i.setClassName("com.sogilis.sogimailer", com.sogilis.sogimailer.SogiMailerService.class.getName());
-		boolean ret = bindService(i, connection, Context.BIND_AUTO_CREATE);
-		Log.i(TAG, "initService() bound value: " + ret);
-	}
-
-	/**
-	 * This is the class which represents the actual service-connection.
-	 * It type casts the bound-stub implementation of the service class to our AIDL interface.
-	 */
-	class AddServiceConnection implements ServiceConnection {
-
-		public void onServiceConnected(ComponentName name, IBinder boundService) {
-			service = IHelloService.Stub.asInterface((IBinder) boundService);
-			Log.i(TAG, "onServiceConnected(): Connected");
-			Toast.makeText(MainActivity.this, "AIDLExample Service connected", Toast.LENGTH_LONG).show();
-		}
-
-		public void onServiceDisconnected(ComponentName name) {
-			service = null;
-			Log.i(TAG, "onServiceDisconnected(): Disconnected");
-			Toast.makeText(MainActivity.this, "AIDLExample Service Connected", Toast.LENGTH_LONG).show();
-		}
+		Log.d(TAG, "onCreate");
+		r = new MailerResult(new Handler());
+		r.setReceiver(new Receiver() {
+			@Override
+			public void onReceiveResult(int resultCode, Bundle resultData) {
+				Log.d(TAG, "onReceiveResult");
+				Toast.makeText(MainActivity.this, "Message : <" + resultData.getString("TITI"), Toast.LENGTH_LONG).show();
+			}
+		});
 	}
 
 	public void click(View v)
 	{
+		Log.d(TAG, "Launching intent for service SogiMailer");
 		EditText value2  = (EditText) findViewById(R.id.edit);
-		try {
-			service.hello(value2.getText().toString());
-		} catch (RemoteException e) {
-			Log.i(TAG, "Data fetch failed with: " + e);
-			e.printStackTrace();
+		Intent itt = new Intent();
+		itt.setClassName("com.sogilis.sogimailer", "com.sogilis.sogimailer.SogiMailerService");
+		itt.putExtra("TOTO", value2.getText().toString());
+		itt.putExtra("TUTU", r);
+
+		startService(itt);
+
+	}
+
+	// Defines our event interface for communication
+	public interface Receiver {
+		public void onReceiveResult(int resultCode, Bundle resultData);
+	}
+
+	private class MailerResult extends ResultReceiver {
+
+		private Receiver receiver;
+
+		public MailerResult(Handler h) {
+			super(h);
 		}
-	}
 
-	private void releaseService() {
-		unbindService(connection);
-		connection = null;
-		Log.d(TAG, "releaseService(): unbound.");
-	}
+		// Setter for assigning the receiver
+		public void setReceiver(Receiver receiver) {
+			this.receiver = receiver;
+		}
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		releaseService();
+		// Delegate method which passes the result to the receiver if the receiver has been assigned
+		@Override
+		protected void onReceiveResult(int resultCode, Bundle resultData) {
+			if (receiver != null) {
+				receiver.onReceiveResult(resultCode, resultData);
+			}
+		}
+
 	}
 }
--- app/src/main/java/com/sogilis/sogimailer/SogiMailerService.java
+++ app/src/main/java/com/sogilis/sogimailer/SogiMailerService.java
@@ -1,14 +1,23 @@
 package com.sogilis.sogimailer;
 
+import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.os.ResultReceiver;
+import android.os.SystemClock;
 import android.util.Log;
 import android.widget.Toast;
 
-public class SogiMailerService extends Service {
+public class SogiMailerService extends IntentService {
 
 	private static final String TAG = "MAILER";
+
+    public SogiMailerService() {
+        // Used to name the worker thread, important only for debugging.
+        super("test-service");
+    }
 
     @Override
     public void onCreate() {
@@ -21,17 +30,15 @@ public class SogiMailerService extends Service {
 	 * for sending messages to the service.
 	 */
 	@Override
-	public IBinder onBind(Intent intent) {
-
-		return new IHelloService.Stub() {
-
-			public void hello(String holy) {
-				Log.d(TAG, "hello in IHelloService impl");
-				Toast.makeText(getApplicationContext(), "Hello ! You told me : <" + holy + ">", Toast.LENGTH_SHORT).show();
-			}
-
-		};
-
+	protected void onHandleIntent(Intent intent) {
+		Log.d(TAG, "hello in IHelloService impl");
+		String holy = intent.getStringExtra("TOTO");
+		ResultReceiver rr = intent.getParcelableExtra("TUTU");
+		if (rr != null) {
+			Bundle b = new Bundle();
+			b.putString("TITI", "oula oula ! " + holy);
+			rr.send(-1, b);
+		}
 	}
 
 	@Override
