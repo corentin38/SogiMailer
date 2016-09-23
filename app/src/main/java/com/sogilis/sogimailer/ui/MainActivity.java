fb40e535db53f2f7290455076c4ac8ea68efe9cc
Corentin Marciau <corentin@sogilis.com>
2016-09-23 15:34:03 +0200

    [DEV] Remove commented code

--- app/src/main/java/com/sogilis/sogimailer/ui/MainActivity.java
+++ app/src/main/java/com/sogilis/sogimailer/ui/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -16,18 +15,14 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 import com.sogilis.sogimailer.R;
 import com.sogilis.sogimailer.SogiMailerApplication;
 import com.sogilis.sogimailer.dude.ProfileDude;
-import com.sogilis.sogimailer.mail.Default;
 import com.sogilis.sogimailer.mail.Mailer;
-import com.sogilis.sogimailer.mail.NoSuchProfileException;
 import com.sogilis.sogimailer.mail.Profile;
 
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -126,142 +121,6 @@ public class MainActivity extends AppCompatActivity {
 
 		return super.onOptionsItemSelected(item);
 	}
-
-/*
-
-	private void addFragment(Fragment frag, String tag) {
-		Log.d(TAG, "addFragment");
-
-
-
-
-		FragmentManager fm = getSupportFragmentManager();
-
-		fm.dump(TAG + " BEFORE !!! ", null, new PrintWriter(System.out, true), null);
-
-
-		FragmentTransaction tx = fm.beginTransaction();
-		tx.replace(R.id.main_fragment_holder, frag, tag);
-		tx.addToBackStack(tag);
-		tx.commit();
-		fm.dump(TAG + " AFTER !!! ", null, new PrintWriter(System.out, true), null);
-	}
-*/
-/*
-	private void handleBackPressed() {
-		Log.d(TAG, "handleBackPressed");
-
-		FragmentManager fm = getSupportFragmentManager();
-		String tag = fm.getBackStackEntryAt(fm.getBackStackEntryCount() -1).getName();
-		Log.d(TAG, "last tag :" + tag);
-		Fragment current = fm.findFragmentByTag(tag);
-
-		Log.d(TAG, "current frag :" + current.getTag());
-
-		if (current instanceof HomeFragment) {
-			Log.d(TAG, "We are at home already, do nothing");
-			return;
-		}
-
-		Log.d(TAG, "let's pop !");
-		fm.popBackStack();
-	}*/
-/*
-
-	public void edit(View view) {
-		Log.d(TAG, "edit");
-		goEdit();
-	}
-
-	public void saveEdit(View view) {
-		Log.d(TAG, "saveEdit");
-
-		FragmentManager fm = getSupportFragmentManager();
-		EditFragment frag = (EditFragment) fm.findFragmentByTag(EditFragment.FRAGMENT_KEY);
-		profileDude.saveBasic(frag.getSenderEntry(), frag.getHostEntry(), frag.getPasswordEntry());
-
-		goHome();
-	}
-
-	public void cancelEdit(View view) {
-		Log.d(TAG, "saveEdit");
-		goHome();
-	}
-
-	public void discardDisclaimer(View view) {
-		Log.d(TAG, "discardDisclaimer");
-		removeDisclaimer();
-	}
-
-	private void goHome() {
-		Log.d(TAG, "goHome");
-
-		Profile profile = new Default();
-		try {
-			profile = profileDude.getBasic();
-		} catch (NoSuchProfileException e) {
-			Log.d(TAG, "No profile defined while getting back home");
-		}
-
-		Fragment home = HomeFragment.newInstance(profile);
-
-		removeDisclaimer();
-		addFragment(home, HomeFragment.FRAGMENT_KEY);
-	}
-
-	private void goEdit() {
-		Log.d(TAG, "goEdit");
-
-		Profile profile = null;
-		try {
-			profile = profileDude.getBasic();
-		} catch (NoSuchProfileException e) {
-			Log.d(TAG, "No profile defined while getting back home");
-			showDisclaimer();
-		}
-		Fragment edit = EditFragment.newInstance(0, profile);
-		addFragment(edit, EditFragment.FRAGMENT_KEY);
-	}
-
-	private void showDisclaimer() {
-		Log.d(TAG, "showDisclaimer");
-		FragmentManager fm = getSupportFragmentManager();
-		Fragment disc = fm.findFragmentByTag(DisclaimerFragment.FRAGMENT_KEY);
-		if (disc == null) {
-			Log.d(TAG, "disclaimer is null");
-			disc = new DisclaimerFragment();
-		}
-
-		FragmentTransaction tx = fm.beginTransaction();
-		tx.replace(R.id.disclaimer_fragment_holder, disc, DisclaimerFragment.FRAGMENT_KEY);
-		tx.addToBackStack(DisclaimerFragment.FRAGMENT_KEY);
-		tx.commit();
-	}
-
-	private void removeDisclaimer() {
-		Log.d(TAG, "removeDisclaimer");
-		FragmentManager fm = getSupportFragmentManager();
-		Fragment disc = fm.findFragmentByTag(DisclaimerFragment.FRAGMENT_KEY);
-		if (disc == null) {
-			Log.d(TAG, "disclaimer is null !!");
-			return;
-		}
-
-		Log.d(TAG, "disclaimer is not null");
-		FragmentTransaction tx = fm.beginTransaction();
-		tx.remove(disc);
-		tx.addToBackStack(DisclaimerFragment.FRAGMENT_KEY);
-		tx.commit();
-	}
-
-	private void goDocumentation() {
-		Log.d(TAG, "goDocumentation");
-		Fragment docu = new DocumentationFragment();
-		addFragment(docu, DocumentationFragment.FRAGMENT_KEY);
-	}
-*/
-
-
 
     static class Adapter extends FragmentPagerAdapter {
         private final List<Fragment> mFragmentList = new ArrayList<>();
