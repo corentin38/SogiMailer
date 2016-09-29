4ed7db349b2fe0e2b418651bc2a38579c28c0486
Corentin Marciau <corentin@sogilis.com>
2016-09-29 17:14:22 +0200

    [DEV] Add contract table for sqlite profile table

---
+++ app/src/main/java/com/sogilis/sogimailer/db/Contract.java
@@ -0,0 +1,23 @@
+package com.sogilis.sogimailer.db;
+
+import android.provider.BaseColumns;
+
+public class Contract {
+
+	public Contract() {}
+
+	public static abstract class Profile implements BaseColumns {
+		public static final String TABLE_NAME                               = "profiles";
+		public static final String COLUMN_NAME_SENDER                       = "sender";
+		public static final String COLUMN_NAME_SENDER_PASSWORD              = "sender_password";
+		public static final String COLUMN_NAME_TRANSPORT_PROTOCOL           = "transport_protocol";
+		public static final String COLUMN_NAME_HOST                         = "host";
+		public static final String COLUMN_NAME_SMTP_QUIT_WAIT               = "smtp_quit_wait";
+		public static final String COLUMN_NAME_SMTP_AUTH                    = "smtp_auth";
+		public static final String COLUMN_NAME_SMTP_PORT                    = "smtp_port";
+		public static final String COLUMN_NAME_SMTP_SOCKET_FACTORY_PORT     = "smtp_socket_factory_port";
+		public static final String COLUMN_NAME_SMTP_SOCKET_FACTORY_CLASS    = "smtp_socket_factory_class";
+		public static final String COLUMN_NAME_SMTP_SOCKET_FACTORY_FALLBACK = "smtp_socket_factory_fallback";
+	}
+
+}
