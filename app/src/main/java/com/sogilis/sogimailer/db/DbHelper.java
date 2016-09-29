dccba49dad5dd7e9b64bcfe7032a762e7e670036
Corentin Marciau <corentin@sogilis.com>
2016-09-29 17:26:32 +0200

    [DEV] Add DbHelper to deal with DB

---
+++ app/src/main/java/com/sogilis/sogimailer/db/DbHelper.java
@@ -0,0 +1,50 @@
+package com.sogilis.sogimailer.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbHelper extends SQLiteOpenHelper {
+
+	private static final String TEXT_TYPE = " TEXT";
+	private static final String COMMA_SEP = ",";
+
+	private static final String SQL_CREATE_PROFILES =
+		"CREATE TABLE " + Contract.Profile.TABLE_NAME + " (" +
+		Contract.Profile._ID                                      + " INTEGER PRIMARY KEY," +
+		Contract.Profile.COLUMN_NAME_SENDER                       + TEXT_TYPE + COMMA_SEP +
+		Contract.Profile.COLUMN_NAME_SENDER_PASSWORD              + TEXT_TYPE + COMMA_SEP +
+		Contract.Profile.COLUMN_NAME_TRANSPORT_PROTOCOL           + TEXT_TYPE + COMMA_SEP +
+		Contract.Profile.COLUMN_NAME_HOST                         + TEXT_TYPE + COMMA_SEP +
+		Contract.Profile.COLUMN_NAME_SMTP_QUIT_WAIT               + TEXT_TYPE + COMMA_SEP +
+		Contract.Profile.COLUMN_NAME_SMTP_AUTH                    + TEXT_TYPE + COMMA_SEP +
+		Contract.Profile.COLUMN_NAME_SMTP_PORT                    + TEXT_TYPE + COMMA_SEP +
+		Contract.Profile.COLUMN_NAME_SMTP_SOCKET_FACTORY_PORT     + TEXT_TYPE + COMMA_SEP +
+		Contract.Profile.COLUMN_NAME_SMTP_SOCKET_FACTORY_CLASS    + TEXT_TYPE + COMMA_SEP +
+		Contract.Profile.COLUMN_NAME_SMTP_SOCKET_FACTORY_FALLBACK + TEXT_TYPE + COMMA_SEP + " )";
+
+	private static final String SQL_DELETE_PROFILES =
+			"DROP TABLE IF EXISTS " + Contract.Profile.TABLE_NAME;
+
+	public static final int DATABASE_VERSION = 1;
+	public static final String DATABASE_NAME = "SogiMailer.db";
+
+	public DbHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(SQL_CREATE_PROFILES);
+	}
+
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// We should probably keep the user data ...
+		db.execSQL(SQL_DELETE_PROFILES);
+		onCreate(db);
+	}
+
+	public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		onUpgrade(db, oldVersion, newVersion);
+	}
+
+}
