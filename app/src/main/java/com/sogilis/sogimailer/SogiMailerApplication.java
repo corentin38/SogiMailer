afbdbddd66e20f6386d69cb4f1468d11f1056cce
Corentin Marciau <corentin@sogilis.com>
2016-10-07 17:31:43 +0200

    [PROJECT] Add Stetho. XXX: Think of removing that before shipping

--- app/src/main/java/com/sogilis/sogimailer/SogiMailerApplication.java
+++ app/src/main/java/com/sogilis/sogimailer/SogiMailerApplication.java
@@ -3,6 +3,8 @@ package com.sogilis.sogimailer;
 import android.app.Application;
 import android.content.Context;
 
+import com.facebook.stetho.Stetho;
+
 import dagger.ObjectGraph;
 
 public class SogiMailerApplication extends Application {
@@ -14,8 +16,9 @@ public class SogiMailerApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-
 		this.ctx = getApplicationContext();
+
+	    Stetho.initializeWithDefaults(this);
 		objectGraph = ObjectGraph.create(new SogiMailerModule());
 	}
 
