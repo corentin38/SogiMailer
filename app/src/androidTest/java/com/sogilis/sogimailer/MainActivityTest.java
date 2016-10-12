a5d487de289be337ce9c787e0385b82508255ff1
Corentin Marciau <corentin@sogilis.com>
2016-10-12 09:56:20 +0200

    [DEV] Wake emulator before running tests

--- app/src/androidTest/java/com/sogilis/sogimailer/MainActivityTest.java
+++ app/src/androidTest/java/com/sogilis/sogimailer/MainActivityTest.java
@@ -1,13 +1,14 @@
 package com.sogilis.sogimailer;
 
 import android.support.test.espresso.contrib.DrawerActions;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.WindowManager;
 
 import com.sogilis.sogimailer.ui.MainActivity;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,6 +25,19 @@ public class MainActivityTest {
 
 	@Rule
 	public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() {
+        final MainActivity activity = mActivityRule.getActivity();
+        Runnable wakeUpDevice = new Runnable() {
+            public void run() {
+                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+                    WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                    WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+        };
+        activity.runOnUiThread(wakeUpDevice);
+    }
 
 	@Test
 	public void drawerTestMenuClicked_openTestDialog() {
